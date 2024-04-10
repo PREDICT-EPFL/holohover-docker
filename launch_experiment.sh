0ef69b06968677470b53559b3bf9b3cd080eb849
Andrea Grillo <andrea.grillo@epfl.ch>
2024-04-10 02:53:17 +0200

    launch_experiment

--- launch_experiment.sh
+++ launch_experiment.sh
@@ -10,11 +10,11 @@ fi
 echo "This machine is: $1"
 echo "Starting"
 
-sudo docker run -it --rm --name holohover \
+sudo docker run --rm --name holohover \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$(pwd)/ws:/root/ros2_ws" \
+    --volume="$(pwd)/holohover-docker/ws:/root/ros2_ws" \
     --volume="$(pwd)/piqp:/root/piqp" \
     --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
     --volume="./.bash_history:/root/.bash_history" \

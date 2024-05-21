e91583b4cba887e706ae5472604a4e89e016495f
Andrea Grillo <andrea.grillo@epfl.ch>
2024-05-21 16:27:46 +0200

    use docker-push-ssh to upload images to the remote machines

--- upload-images.sh
+++ upload-images.sh
@@ -1,5 +1,10 @@
-sudo docker save holohover | ssh -C ubuntu@192.168.0.71 sudo docker load &
-sudo docker save holohover | ssh -C ubuntu@192.168.0.72 sudo docker load &
-sudo docker save holohover-light-aa | ssh -C ubuntu@192.168.0.140 docker load &
-sudo docker save holohover-light-aa | ssh -C pi@192.168.0.113 docker load &
+docker-push-ssh ubuntu@192.168.0.71 holohover -i ~/.ssh/id_ed25519
+docker-push-ssh ubuntu@192.168.0.71 holohover -i ~/.ssh/id_ed25519
+docker-push-ssh ubuntu@192.168.0.140 holohover-light-aa -i ~/.ssh/id_ed25519
+docker-push-ssh pi@192.168.0.113 holohover-light-aa -i ~/.ssh/id_ed25519
+
+#sudo docker save holohover | ssh -C ubuntu@192.168.0.71 sudo docker load &
+#sudo docker save holohover | ssh -C ubuntu@192.168.0.72 sudo docker load &
+#sudo docker save holohover-light-aa | ssh -C ubuntu@192.168.0.140 docker load &
+#sudo docker save holohover-light-aa | ssh -C pi@192.168.0.113 docker load &
 wait

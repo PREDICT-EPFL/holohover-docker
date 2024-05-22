3dcaa0e287da83b9ec643bdb1c51ad263fb9588c
Andrea Grillo <andrea.grillo@epfl.ch>
2024-05-22 11:01:03 +0200

    add nice capabilities to container

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ echo $0" - Starting the container."
 touch .bash_history
 
 sudo docker run -it --rm --name holohover \
+    --cap-add=SYS_NICE \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \

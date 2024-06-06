e452cbe3848a85dd7955919e8eceb7be3f38d907
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-06 11:31:48 +0200

    updated launch trajectory to work with service

--- remote-launch/launch_trajectory.sh
+++ remote-launch/launch_trajectory.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo docker exec -it holohover /bin/bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 run holohover_dmpc trajectory_generator --ros-args -p trajectory_file:='/root/ros2_ws/src/holohover/holohover_dmpc/config/trajectories/$1'"
+FILE="/root/ros2_ws/src/holohover/holohover_dmpc/config/trajectories/$1"
+
+
+echo "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 service call /trajectory_generator holohover_msgs/srv/TrajectoryGeneratorTrigger \"{filename: {data: \"$FILE\"}}"
+sudo docker exec -it holohover /bin/bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 service call /trajectory_generator holohover_msgs/srv/TrajectoryGeneratorTrigger \"{filename: {data: \"$FILE\"}}\""

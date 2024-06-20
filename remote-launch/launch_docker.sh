3a5fcf55562456a719c5029161a7222ee0cc287f
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-20 10:32:15 +0200

    change in launch

--- remote-launch/launch_docker.sh
+++ remote-launch/launch_docker.sh
@@ -13,20 +13,22 @@ if [ "$#" -ne 4 ]; then
     echo "Usage:"
     echo "   $0 IMAGE_NAME LAUNCH_FILE EXPERIMENT_FILE MACHINE_NAME"
     exit -1
-fi
+fi 
 
 echo "This machine is: $4"
 echo "Image: $1"
 echo "Starting"
 
-sudo docker run --rm --name $1 \
-    --cap-add=SYS_NICE \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$DIRECTORY/log:/root/ros2_ws/log" \
-    --volume="$DIRECTORY/ws/src:/root/ros2_ws/src" \
-    --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
-    --volume="$DIRECTORY/.bash_history:/root/.bash_history" \
-    --network host \
-    $1 bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
+sudo docker exec -it $1 "/bin/bash export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
+
+# sudo docker run --rm --name $1 \
+#     --cap-add=SYS_NICE \
+#     --env="DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#     --volume="$DIRECTORY/log:/root/ros2_ws/log" \
+#     --volume="$DIRECTORY/ws/src:/root/ros2_ws/src" \
+#     --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
+#     --volume="$DIRECTORY/.bash_history:/root/.bash_history" \
+#     --network host \
+#     $1 bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"

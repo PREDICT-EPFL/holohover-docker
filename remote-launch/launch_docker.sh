31c65bcff62f6219dc932b9ec8f9984485c688d9
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-26 19:09:27 +0200

    use fastdds server

--- remote-launch/launch_docker.sh
+++ remote-launch/launch_docker.sh
@@ -20,7 +20,7 @@ echo "Image: $1"
 echo "Starting"
 
 if docker ps --filter "name=$1" --filter "status=running" | grep -q $1; then
-    sudo docker exec $1 /bin/bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
+    sudo docker exec $1 /bin/bash -c "export ROS_DOMAIN_ID=123 && export ROS_DISCOVERY_SERVER=192.168.0.70:11811 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
 else
     sudo docker run --rm --name $1 \
     --privileged \
@@ -33,5 +33,5 @@ else
     --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
     --volume="$DIRECTORY/.bash_history:/root/.bash_history" \
     --network host \
-    $1 bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
+    $1 bash -c "export ROS_DOMAIN_ID=123 && export ROS_DISCOVERY_SERVER=192.168.0.70:11811 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils $2 experiment:='$3' machine:='$4' record:='true'"
 fi

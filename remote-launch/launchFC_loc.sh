a3e49dc8e2c9c0544b332ba1b881130b160c2b68
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-26 19:18:47 +0200

    launchFC

--- remote-launch/launchFC_loc.sh
+++ remote-launch/launchFC_loc.sh
@@ -15,7 +15,7 @@ echo "Starting controller"
 
 	
 if docker ps --filter "name=holohover-light-aa" --filter "status=running" | grep -q holohover-light-aa; then
-    sudo docker exec holohover-light-aa /bin/bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils embedded_controller.launch.py name:=$1"
+    sudo docker exec holohover-light-aa /bin/bash -c "export ROS_DOMAIN_ID=123 && export ROS_DISCOVERY_SERVER=192.168.0.70:11811 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils embedded_controller.launch.py name:=$1"
 else
     sudo docker run --rm --name holohover-light-aa \
     --privileged \
@@ -28,5 +28,5 @@ else
     --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
     --volume="$DIRECTORY/.bash_history:/root/.bash_history" \
     --network host \
-    holohover-light-aa bash -c "export ROS_DOMAIN_ID=123 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils embedded_controller.launch.py name:=$1"
+    holohover-light-aa bash -c "export ROS_DOMAIN_ID=123 && export ROS_DISCOVERY_SERVER=192.168.0.70:11811 && source /opt/ros/humble/setup.bash && source /root/ros2_ws/install/local_setup.bash && ros2 launch holohover_utils embedded_controller.launch.py name:=$1"
 fi

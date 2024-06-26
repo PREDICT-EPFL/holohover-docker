494fb77d8de0b4c9df1c8de5490bd938b75e10e5
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-26 13:20:00 +0200

    shutdownAll for fc remote ips

--- remote-launch/shutdownAll.sh
+++ remote-launch/shutdownAll.sh
@@ -3,8 +3,8 @@
 source ./config.sh
 
 
-for ((i = 0; i < ${#REMOTE_IPS[@]}; i++)); do
-    if ! [ "${REMOTE_IPS[i]}" = "localhost" ]; then
-        ssh ${REMOTE_IPS[i]} sudo init 0
+for ((i = 0; i < ${#FC_REMOTE_IPS[@]}; i++)); do
+    if ! [ "${FC_REMOTE_IPS[i]}" = "localhost" ]; then
+        ssh ${FC_REMOTE_IPS[i]} sudo init 0
     fi
 done

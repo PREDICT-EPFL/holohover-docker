7260ce12ff55ba387a13899a447a2d27b3c5b33e
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-27 16:43:19 +0200

    shutdown in parallel

--- remote-launch/shutdownAll.sh
+++ remote-launch/shutdownAll.sh
@@ -5,6 +5,7 @@ source ./config.sh
 
 for ((i = 0; i < ${#FC_REMOTE_IPS[@]}; i++)); do
     if ! [ "${FC_REMOTE_IPS[i]}" = "localhost" ]; then
-        ssh ${FC_REMOTE_IPS[i]} sudo init 0
+        ssh ${FC_REMOTE_IPS[i]} sudo init 0 &
     fi
 done
+wait

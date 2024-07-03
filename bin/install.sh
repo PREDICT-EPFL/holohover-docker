060d04b6b998f7ed26b20057939a267b83ae39ef
Andrea Grillo <andrea.grillo@epfl.ch>
2024-07-03 16:29:45 +0200

    removed qpoases, added quill

--- bin/install.sh
+++ bin/install.sh
@@ -7,10 +7,9 @@ curr_dir=$(pwd)
 # move to home directory
 cd
 
-echo "Installing CASADI..."
-git clone https://github.com/casadi/casadi.git casadi
-cd casadi
-git checkout "$CASADI_VERSION"
+echo "Installing QUILL..."
+git clone http://github.com/odygrd/quill.git quill
+cd quill
 
 mkdir build
 cd build
@@ -18,10 +17,12 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j12
 sudo make install
 
+cd
 
-echo "Installing qpOASES"
-git clone https://github.com/coin-or/qpOASES.git
-cd qpOASES
+echo "Installing CASADI..."
+git clone https://github.com/casadi/casadi.git casadi
+cd casadi
+git checkout "$CASADI_VERSION"
 
 mkdir build
 cd build

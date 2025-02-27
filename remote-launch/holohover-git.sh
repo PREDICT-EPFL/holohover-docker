c80f1f23439a9296634e1048c41cbb248af93879
Andrea Grillo <andrea.grillo@epfl.ch>
2025-02-27 23:25:15 +0100

    make update scripts checkout current branch

--- remote-launch/holohover-git.sh
+++ remote-launch/holohover-git.sh
@@ -2,11 +2,14 @@
 
 source "$(dirname "$0")/config.sh"
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch is $current_branch"
+
 for ((i = 0; i < ${#FC_REMOTE_IPS[@]}; i++)); do
     echo "Pinging ${FC_REMOTE_IPS[i]}..."
     if ping -c 1 ${FC_REMOTE_IPS[i]} &> /dev/null; then
         echo "Machine ${FC_REMOTE_IPS[i]} is reachable. Pulling latest code..."
-        ssh ${FC_REMOTE_IPS[i]} "cd holohover-docker && git pull"
+        ssh ${FC_REMOTE_IPS[i]} "cd holohover-docker && git checkout $current_branch && git pull"
     else
         echo "Machine ${FC_REMOTE_IPS[i]} is not reachable."
     fi
@@ -16,7 +19,7 @@ for ((i = 0; i < ${#REMOTE_IPS[@]}; i++)); do
     echo "Pinging ${REMOTE_IPS[i]}..."
     if ping -c 1 ${REMOTE_IPS[i]} &> /dev/null; then
         echo "Machine ${REMOTE_IPS[i]} is reachable. Pulling latest code..."
-        ssh ${REMOTE_IPS[i]} "cd holohover-docker && git pull"
+        ssh ${REMOTE_IPS[i]} "cd holohover-docker && git checkout $current_branch && git pull"
     else
         echo "Machine ${REMOTE_IPS[i]} is not reachable."
     fi

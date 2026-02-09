786cd48d9aef49735ea12de92ffe5d82084ff975
Karlym <karlym5@gmail.com>
2026-02-09 10:30:37 +0100

    removed docker run flags that break ros2 dds

--- internal/run_docker.sh
+++ internal/run_docker.sh
@@ -1,8 +1,5 @@
 
 docker run -it --rm --name $1 \
-    --env ROS_DOMAIN_ID=123 \
-    --env ROS_DISCOVERY_SERVER=192.168.0.70:11811 \
-    --env FASTRTPS_DEFAULT_PROFILES_FILE=/root/fastrtps_profiles_superclient.xml \
     --privileged \
     --cap-add=SYS_NICE \
     --env="DISPLAY" \
@@ -12,5 +9,4 @@ docker run -it --rm --name $1 \
     --volume="$(pwd)/ws/src:/root/ros2_ws/src" \
     --volume="/home/$USER/.Xauthority:/root/.Xauthority" \
     --volume="./.bash_history:/root/.bash_history" \
-    --network host \
     $@ 
